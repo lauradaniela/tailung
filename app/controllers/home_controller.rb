28d125def2bc268be7463ada466283e7128b3fb4
Julian Vargas <julianvargasalvarez@gmail.com>
2018-03-12 20:25:00 -0500

    cleanup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,5 @@
-class HomeViewModelGenerator
-  def generate_view_model
-    if curren_user.tester
-      {title: 'hola Meli'}
-    else
-      {title: 'hola normal'}
-    end
-  end
-end
-
-
 class HomeController < ApplicationController
   def index
-    render component: "#{controller_name}/#{action_name}", props: HomeViewModelGenerator.new.generate_view_model
+    render component: "#{controller_name}/#{action_name}", props: {title: "hola mundo"}
   end
-
-  private
 end
